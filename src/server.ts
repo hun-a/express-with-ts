869359e3f2be17021b83b33a5853692e3af6f57b
hun-a <seunghun.chan@gmail.com>
2020-04-23 15:15:40 +0900

    Add my first typed middleware

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,13 @@
 import * as express from 'express';
 
+function loggerMiddleware(request: express.Request, response: express.Response, next) {
+  console.log(`${request.method} ${request.path}`);
+  next();
+}
+
 const app = express();
+
+app.use(loggerMiddleware);
 
 app.get('/', (req, res) => {
   res.send('Hello world!');
