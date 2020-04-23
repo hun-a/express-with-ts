0b3240e2d7d957697c483bcdaf055f6666d5ab61
hun-a <seunghun.chan@gmail.com>
2020-04-23 17:18:23 +0900

    Add the connecting to the MongoDB by mongoose and dotenv

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,23 @@
+import * as mongoose from 'mongoose';
+import 'dotenv/config';
 import App from './app';
 import PostsController from './posts/posts.controller';
 
+const {
+    MONG_USER,
+    MONG_PASSWORD,
+    MONGO_PATH,
+    PORT
+} = process.env;
+
+mongoose.connect(
+  `mongodb+srv://${MONG_USER}:${MONG_PASSWORD}${MONGO_PATH}`,
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
+
 const app = new App(
   [ new PostsController() ],
-  5000,
+  Number(PORT),
 );
 
 app.listen();
