aecc862d0b8ac6c5888457800870a10c6884af00
hun-a <seunghun.chan@gmail.com>
2020-04-23 15:12:08 +0900

    Create the entry point of express server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,9 @@
-console.log("Hello typescript!");
+import * as express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello world!');
+});
+
+app.listen(5000);
